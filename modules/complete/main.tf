bcfd819fa907f6542254bcde327320e4df272c53
Bernardo Lopes <bernardo@linuxadmin.cloud>
2023-04-27 21:06:41 -0300

    hotfix: providers

--- modules/complete/main.tf
+++ modules/complete/main.tf
@@ -13,6 +13,11 @@ terraform {
   }
 }
 
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_id
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
@@ -20,14 +25,14 @@ provider "aws" {
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
-    token                  = module.eks.cluster_token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
   }
 }
 provider "kubectl" {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
-    token                  = module.eks.cluster_token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
 }
 
 module "eks" {
